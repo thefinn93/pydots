1c503e7e023a230d67776121b7a14305e28eec85
Finn <finn@finn.io>
2015-03-15 13:50:20 -0700

    fixed width ftw

--- dots.py
+++ dots.py
@@ -16,11 +16,11 @@ def genhtml(path=None):
         title = "Hello, crawlers!"
     html = "<html>\n<head>\n<title>%s</title>\n</head><body>" % title
     html += "Dots, for crawlers that don't read the <a href=\"/robots.txt\">robots.txt</a>"
-    html += "<br /><ul>"
+    html += "<br />\n<code>\n<ul>"
     for i in range(0, LISTSIZE):
         rand = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(DOTSLENGTH))
         html += "<li><a href=\"/dots/%s\">%s</a></li>" % (rand, rand)
-    html += "</ul>\n</body>\n</head>"
+    html += "</ul>\n</code>\n</body>\n</head>"
     return html
 
 
