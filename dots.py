5395e1dd48d574642df0fdf20572ab86e3fd4ddd
Finn <finn@finn.io>
2015-03-15 14:19:36 -0700

    More random

--- dots.py
+++ dots.py
@@ -7,8 +7,8 @@ import HTMLParser
 from flask import Flask
 app = Flask(__name__)
 
-LISTSIZE = 50
-DOTSLENGTH = 50
+LISTSIZE = random.randint(25, 100)
+PATHSIZE = (10, 100)
 
 html_escape_table = {
     "&": "&amp;",
@@ -21,8 +21,7 @@ html_escape_table = {
 
 def html_escape(text):
     """Produce entities within text."""
-    return "".join(html_escape_table.get(c,c) for c in text)
-
+    return "".join(html_escape_table.get(c, c) for c in text)
 
 
 def genhtml(path=None):
@@ -32,7 +31,7 @@ def genhtml(path=None):
     html = "<html>\n<head>\n<title>%s</title>\n</head><body>" % title
     html += "Dots, for crawlers that don't read the <a href=\"/robots.txt\">robots.txt</a>"
     html += "<br />\n<code>\n<ul>"
-    for i in range(0, LISTSIZE):
+    for i in range(0, random.randint(PATHSIZE[0], PATHSIZE[1])):
         rand = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(DOTSLENGTH))
         html += "<li><a href=\"/dots/%s\">%s</a></li>" % (rand, rand)
     html += "</ul>\n</code>\n</body>\n</head>"
